28cffa0dea451a2d3ddab9468c611cafc43cfa6a
Justin May <may.justin@gmail.com>
2014-05-17 20:34:00 -0700

    Added more tests

--- test/ci_seer/seers/jenkins/seer_test.clj
+++ test/ci_seer/seers/jenkins/seer_test.clj
@@ -28,14 +28,37 @@
     (let [jobs (seers/get-jobs-in-folder jenkins/seer mock-context nil)]
       (testing "Parsing JSON payload into a jobs list."
         (is (= 12 (count jobs)))
-        (letfn [(job [name] (seers/find-job-by-name jobs name))]
-          (is (= :passing (:status (job "job1"))))
-          (is (:running (job "job1")))
-          (is (not (:running (job "job2"))))
-          (is (= :passing (:status (job "job3"))))
-          (is (= :aborted (:status (job "job4"))))
-          (is (= :pending (:status (job "job5"))))
-          (is (= :failing (:status (job "job6"))))
-          (is (= :unstable (:status (job "job7"))))
-          (is (= :passing (:status (job "job8"))))
-          (is (= :passing (:status (job "job9")))))))))
+        (letfn [(get-job [name] (seers/find-job-by-name jobs name))]
+
+          (testing "Parsing :running attribute"
+            (is (:running (get-job "job1")))
+            (is (not (:running (get-job "job2")))))
+
+          (testing "Parsing job statuses."
+            (is (= :passing (:status (get-job "job1"))))
+            (is (= :disabled (:status (get-job "job2"))))
+            (is (= :passing (:status (get-job "job3"))))
+            (is (= :aborted (:status (get-job "job4"))))
+            (is (= :pending (:status (get-job "job5"))))
+            (is (= :failing (:status (get-job "job6"))))
+            (is (= :unstable (:status (get-job "job7"))))
+            (is (= :passing (:status (get-job "job8"))))
+            (is (= :passing (:status (get-job "job9"))))
+            (is (= :disabled (:status (get-job "job10"))))
+            (is (= :passing (:status (get-job "job11"))))
+            (is (= :notbuilt (:status (get-job "job12")))))
+
+          (testing "Parsing :label attribute."
+            (is (= "job1" (:label (get-job "job1"))))
+            (is (= "job2" (:label (get-job "job2"))))
+            (is (= "job3" (:label (get-job "job3"))))
+            (is (= "job4" (:label (get-job "job4"))))
+            (is (= "job5" (:label (get-job "job5"))))
+            (is (= "job6" (:label (get-job "job6"))))
+            (is (= "job7" (:label (get-job "job7"))))
+            (is (= "job8" (:label (get-job "job8"))))
+            (is (= "job9" (:label (get-job "job9"))))
+            (is (= "job10" (:label (get-job "job10"))))
+            (is (= "job11" (:label (get-job "job11"))))
+            (is (= "job12" (:label (get-job "job12"))))))))))
+
