cf36cfb40cc46fbc2c345a6d8eb67834b0087305
Justin May <may.justin@gmail.com>
2014-06-15 17:31:11 -0700

    Refactored development namespace

--- dev/user.clj
+++ dev/user.clj
@@ -1,10 +1,18 @@
 (ns user
   (:import (java.net URL))
   (:require [clojure.pprint :refer (pprint)]
+            [clojure.repl :refer :all]
             [ci-seer.services.ci-seer.service :refer [seer-service]]
             [clojure.tools.namespace.repl :refer (refresh)]
             [puppetlabs.trapperkeeper.core :as tk]
             [puppetlabs.trapperkeeper.app :as tkapp]))
+
+(def test-config
+  "Trapperkeeper config used for CI-Seer development."
+  {:seer  {:seers ["ci-seer.seers.jenkins/seer"]
+           :servers [{:type "jenkins"
+                      :url "https://jenkins.puppetlabs.com"
+                      :folders ["clojure"]}]}})
 
 (def system nil)
 
@@ -12,12 +20,7 @@
   (alter-var-root
     #'system
     (fn [_]
-      (let [app (tk/build-app
-                  [seer-service]
-                  {:seer  {:seers [ "ci-seer.seers.jenkins/seer" ]
-                           :servers [{:type "jenkins"
-                                      :url "https://jenkins.puppetlabs.com"
-                                      :folders ["clojure"]}]}})]
+      (let [app (tk/build-app [seer-service] test-config)]
         (tkapp/init app))))
   (alter-var-root #'system tkapp/init)
   (tkapp/check-for-errors! system))
@@ -46,8 +49,7 @@
   (stop)
   (refresh :after 'user/go))
 
-(defn jenkins-context
-  [url]
+(defn jenkins-context [url]
   {:url (URL. url)
    :type :jenkins})
 
