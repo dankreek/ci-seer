14e92fdc8c12c2fc69df5ab22a39d3e20f8c1c70
Justin May <may.justin@gmail.com>
2014-10-14 19:52:08 -0700

    Did some dep version bumping
    I had to put this down for the summer, it's just too nice up here in
    Portland to be indoors.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def tk-version "0.4.2")
+(def tk-version "0.5.1")
 
 (defproject dankreek/ci-seer "0.1.0-SNAPSHOT"
   :description "A Clojure-based CI radiator"
@@ -9,12 +9,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [puppetlabs/trapperkeeper ~tk-version]
                  [cheshire "5.3.1"]
-                 [prismatic/schema "0.2.2"]
-                 [clj-http "0.9.1"]
-                 [clj-time "0.7.0"]]
+                 [prismatic/schema "0.3.0"]
+                 [clj-http "1.0.0"]
+                 [clj-time "0.8.0"]]
 
   :main puppetlabs.trapperkeeper.main
 
